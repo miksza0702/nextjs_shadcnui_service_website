0dc84af86f7e9b68a1eff4fd49518686266b329e
miksza0702 <miki.szalacha@gmail.com>
2024-11-06 13:06:02 +0100

    Poprawiony formularz edycji sprzetu

--- pages/equipment/edit/equipment/[equipmentId].tsx
+++ pages/equipment/edit/equipment/[equipmentId].tsx
@@ -2,89 +2,276 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { db } from "../../../../firebase";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+interface EquipmentData {
+    name: string;
+    business: string;
+    type: string;
+    serialNumber: string;
+    location: string;
+    inventoryNumber: string;
+    dateOfPurchase: string;
+    sellingCompany: string;
+    warranty: string; 
+};
 
 
-const EditEquimpent = () => {
+const UpdateEquipment = () => {
     const router = useRouter();
     const { equipmentId } = router.query;
+    const [initialData, setInitialData] = useState<EquipmentData | null>(null);
 
-    const [name, setName] = useState("");
-    const [business, setBusiness] = useState("");
-    const [model, setModel] = useState("");
-    const [serialNumber, setSerialNumber] = useState("");
-    const [location, setLocation] = useState("");
+    const formSchema = z.object({
+        name: z.string().nonempty("Nazwa sprzętu jest wymagana"),
+        business: z.string().nonempty("Nazwa firmy urządzenia jest wymagana"),
+        type: z.string().nonempty("Nazwa modelu urządzenia jest wymagana"),
+        serialNumber: z.string().nonempty("Numer seryjny urządzenia jest wymagany"),
+        location: z.string().nonempty("Lokalizacja urządzenia jest wymagana"),
+        inventoryNumber: z.string().nonempty("Numer inwentarzowy urządzenia jest wymagany"),
+        dateOfPurchase: z.string().nonempty("Data zakupu urządzenia jest wymagana"),
+        sellingCompany: z.string().nonempty("Nazwa firmy, skąd zakupiono urządzenie jest wymagana"),
+        warranty: z.string().nonempty("Data zakończenia gwarancji urządzenia jest wymagana"),
+    });
+
+    type EquipmentFormData = z.infer<typeof formSchema>;
+
+    const form = useForm<EquipmentFormData>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: initialData?.name || "",
+            business: initialData?.business || "",
+            type: initialData?.type || "",
+            serialNumber: initialData?.serialNumber || "",
+            location: initialData?.location || "",
+            inventoryNumber: initialData?.inventoryNumber || "",
+            dateOfPurchase: initialData?.dateOfPurchase || "",
+            sellingCompany: initialData?.sellingCompany || "",
+            warranty: initialData?.warranty || "",            
+        },
+    });
 
     useEffect(() => {
-            const fetchEquipment = async () => {
-                if(!equipmentId) return;
+        const fetchEquipmentData = async () => {
+            if(equipmentId) {
+                const equipmentRef = doc(db, "equipment", equipmentId as string);
+                const docSnap = await getDoc(equipmentRef);
 
-                try {
-                    const docRef = doc(db, "equipment", equipmentId as string);
-                    const docSnap = await getDoc(docRef);
-
-                    if (docSnap.exists()) {
-                        const data = docSnap.data();
-                        setName(data.name || "");
-                        setBusiness(data.business || "");
-                        setModel(data.model || "");
-                        setSerialNumber(data.serialNumber || "");
-                        setLocation(data.location || "");                     
-                    } 
-                } catch (error){
-                    console.error("Błąd pobierania danych sprzętu")
+                if (docSnap.exists()) {
+                    const data = docSnap.data() as EquipmentData;
+                    setInitialData(data);
+                    form.reset(data);
+                } else {
+                    console.error("Sprzęt nie został znaleziony.");
                 }
-            };
-            fetchEquipment();
-        }, [equipmentId]);
+            }
+        };
 
-    const handleUpdateEquipment = async () => {
-        if (!name || !business || !model || !serialNumber || !location){
-            alert("Proszę uzupełnić wszystkie pola.");
-            return;
-        }
+        fetchEquipmentData();
+    }, [equipmentId, form]);
 
-        try {
-            const docRef = doc(db, "equipment", equipmentId as string);
-            await updateDoc(docRef, {
-                name,
-                business,                
-                model,
-                serialNumber,
-                location,
-            });
-            router.push(`/equipment/`);
-        } catch (error) {
-            console.error("Błąd podczas aktualizacji sprzętu", error);
-            alert("Wystąpił błąd podczas aktualizacji. Spróbuj ponownie.");
-        }
+    const updateEquipment = async (data: EquipmentFormData) => {
+        if (!equipmentId) return;
+
+        const equipmentRef = doc(db, "equipment", equipmentId as string);
+        await updateDoc(equipmentRef, {
+            ...data,
+        });
+
+        router.push(`/equipment/${equipmentId}`);
     };
+
+    const handleSubmit = async (data: EquipmentFormData) => {
+        await updateEquipment(data);
+    };
+
+    if (!initialData) {
+        return <div>Ładowanie danych...</div>;
+    }
 
     return (
         <div>
-            <h1>Edytuj sprzęt</h1>
-            <div>
-                <label>Name of device</label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name of device"/>
-            </div>
-            <div>
-                <label>Business</label>
-                <input type="text" value={business} onChange={(e) => setBusiness(e.target.value)} placeholder="Business"/>
-            </div>
-            <div>
-                <label>Model</label>
-                <input type="text" value={model} onChange={(e) => setModel(e.target.value)} placeholder="Model"/>
-            </div>
-            <div>
-                <label>Serial Number</label>
-                <input type="text" value={serialNumber} onChange={(e) => setSerialNumber(e.target.value)} placeholder="SN"/>
-            </div>
-            <div>
-                <label>Location</label>
-                <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Location"/>
-            </div>
-            <button onClick={handleUpdateEquipment}>Zapisz zmiany</button>
+            <h1 className="text-2xl font-bold mb-4">Edytuj sprzęt</h1>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4 p-6 bg-white shadow-md rounded-md">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nazwa urządzenia</FormLabel>
+                            <FormControl>
+                                <Input placeholder="name" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    
+
+                    <FormField
+                        control={form.control}
+                        name="business"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Nazwa firmy urządzenia</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="business" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="type"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Model urządzenia</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="type" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="serialNumber"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>SN urządzenia</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="serialNumber" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="location"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Lokalizacja urządzenia</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="location" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="inventoryNumber"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Numer inwentarzowy urządzenia</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="inventoryNumber" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="dateOfPurchase"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Data zakupu sprzętu</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="dateOfPurchase" type="date" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="sellingCompany"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Nazwa firmy, gdzie zakupiono sprzęt</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="sellingCompany" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="warranty"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Czas gwarancji</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="warranty" type="date" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                            
+                        )}
+                    />
+                    <Button type="submit" variant="secondary" className="w-full">
+                        Edytuj urządzenie
+                    </Button>
+                </form>
+            </Form>
         </div>
     );
+
 };
 
-export default EditEquimpent;
+export default UpdateEquipment;
+
+
+
+
